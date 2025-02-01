13ebc16d884b39769c6395f2dee92ec29d291714
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-01 20:44:14 +0200

    Add _printAnswersArray private static method in utils header file

--- src/shared/utils/utils.h
+++ src/shared/utils/utils.h
@@ -2,12 +2,18 @@
 #define UTILS_H
 
 #include "../enums/enums.h"
+#include <algorithm>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 class Utils {
 public:
     static RunMode getAppRunMode(int argc, char* argv[]);
+private:
+    static std::string _printAnswersArray(const std::vector<std::string>& validAnswers);
 };
 
 #endif // UTILS_H
