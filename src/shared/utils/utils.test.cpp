63bdd7fa313a9ac2e07adcce1d08a3a3f99c7e0e
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-02 09:08:17 +0200

    Add split static method unit tests

--- src/shared/utils/utils.test.cpp
+++ src/shared/utils/utils.test.cpp
@@ -24,3 +24,35 @@ TEST(GetAppRunModeTest, given_argv_with_invalid_mode_then_returns_normal) {
     EXPECT_EQ(RunMode::NORMAL, Utils::getAppRunMode(argc, argv));
 }
 
+TEST(SplitTest, given_string_with_defalt_delimiter_then_returns_vector_of_strings) {
+    std::string input = R"(A
+B
+C)";
+    std::vector<std::string> expected = {"A", "B", "C"};
+    auto output = Utils::split(input);
+    EXPECT_EQ(output.size(), expected.size());
+    for (size_t i = 0; i < output.size(); i++) {
+        EXPECT_EQ(output[i], expected[i]);
+    }
+}
+
+TEST(SplitTest, given_string_with_custom_delimiter_then_returns_vector_of_one_string) {
+    std::string input = "A,B,C";
+    std::vector<std::string> expected = {"A,B,C"};
+    auto output = Utils::split(input, ' ');
+    EXPECT_EQ(output.size(), expected.size());
+    for (size_t i = 0; i < output.size(); i++) {
+        EXPECT_EQ(output[i], expected[i]);
+    }
+}
+
+TEST(SplitTest, given_string_with_custom_delimiter_then_returns_vector_of_strings) {
+    std::string input = "A,B,C";
+    std::vector<std::string> expected = {"A", "B", "C"};
+    auto output = Utils::split(input, ',');
+    EXPECT_EQ(output.size(), expected.size());
+    for (size_t i = 0; i < output.size(); i++) {
+        EXPECT_EQ(output[i], expected[i]);
+    }
+}
+
