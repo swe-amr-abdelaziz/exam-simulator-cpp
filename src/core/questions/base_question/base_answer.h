5ba24287c8fa9e610084461d27c81009f359de88
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-04 21:16:18 +0200

    Add base_answer.h

---
+++ src/core/questions/base_question/base_answer.h
@@ -0,0 +1,20 @@
+#ifndef BASE_ANSWER_H
+#define BASE_ANSWER_H
+
+#include <optional>
+
+template <class T, class U>
+class Answer {
+public:
+    virtual ~Answer() = default;
+    virtual void setText(T text) = 0;
+    virtual std::optional<T> getText() = 0;
+    virtual void setDegree(U degree) = 0;
+    virtual std::optional<U> getDegree() = 0;
+
+protected:
+    std::optional<T> text = std::nullopt;
+    std::optional<U> degree = std::nullopt;
+};
+
+#endif // BASE_ANSWER_H
