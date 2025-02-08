560952eee2e82aa9232843729a5361a39d479c61
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-08 09:26:08 +0200

    Create a new file test_defaults.h to contain test cases constants

---
+++ src/shared/constants/test_defaults.h
@@ -0,0 +1,14 @@
+#ifndef TEST_DEFAULTS_H
+#define TEST_DEFAULTS_H
+
+#include <string>
+#include <vector>
+
+namespace TestDefaults {
+
+static std::string mcqText = "What is the name of the first month of the year?";
+static std::vector<std::string> choices = {"January", "February", "March", "April", "May"};
+
+} // namespace TestDefaults
+
+#endif // TEST_DEFAULTS_H
