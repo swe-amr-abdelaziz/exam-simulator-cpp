570b0037c4f095dbd8c275a5b4ad70f9b1fa6fcf
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-09 08:39:35 +0200

    Add to test_defaults.h

--- src/shared/constants/test_defaults.h
+++ src/shared/constants/test_defaults.h
@@ -6,8 +6,13 @@
 
 namespace TestDefaults {
 
-static std::string mcqText = "What is the name of the first month of the year?";
-static std::vector<std::string> choices = {"January", "February", "March", "April", "May"};
+const std::string ESSAY_ANSWER_1_LOWERCASE = "i am fine, thanks";
+const std::string ESSAY_ANSWER_1_UPPERCASE = "I AM FINE, THANKS";
+const std::string ESSAY_QUESTION_1 = "How are you?";
+const std::string BOOL_QUESTION_1 = "Are you a software engineer?";
+const std::vector<std::string> BOOL_QUESTION_VALID_ANSWERS_WORDS = {"yes", "no"};
+const std::string MCQ_TEXT = "What is the name of the first month of the year?";
+const std::vector<std::string> CHOICES = {"January", "February", "March", "April", "May"};
 
 } // namespace TestDefaults
 
