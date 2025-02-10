38c607d00a969a5422c5ddbc8e75b83ad4e67990
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-10 13:22:54 +0200

    Modify MultipleChoiceQuestion unit tests to debug the CI

--- src/core/questions/multiple_choice_question/question/multiple_choice_question.test.cpp
+++ src/core/questions/multiple_choice_question/question/multiple_choice_question.test.cpp
@@ -4,13 +4,11 @@
 // original one
 
 // Static const variables
-static const std::unique_ptr<MultipleChoiceAnswer> CORRECT_ANSWER =
-    MultipleChoiceAnswerBuilder::create()
-        .setText(MCQ_CORRECT_ANSWER_TEXT)
-        .setDegree(MCQ_CORRECT_ANSWER_DEGREE)
-        .build();
-static const std::unique_ptr<MultipleChoiceAnswer> STUDENT_ANSWER =
-    MultipleChoiceAnswerBuilder::create().build();
+static const auto CORRECT_ANSWER = MultipleChoiceAnswerBuilder::create()
+                                       .setText(MCQ_CORRECT_ANSWER_TEXT)
+                                       .setDegree(MCQ_CORRECT_ANSWER_DEGREE)
+                                       .build();
+static const auto STUDENT_ANSWER = MultipleChoiceAnswerBuilder::create().build();
 static const uint8_t TEXT_NEW = 1;
 static const double DEGREE_NEW = 5.0;
 static const auto QUESTION = MultipleChoiceQuestionBuilder::create()
@@ -19,7 +17,3 @@ static const auto QUESTION = MultipleChoiceQuestionBuilder::create()
                                  .setStudentAnswer(std::move(STUDENT_ANSWER->clone()))
                                  .setChoices(CHOICES)
                                  .build();
-
-TEST(GetTextTest, should_return_question_text_when_text_is_set) {
-    EXPECT_EQ(QUESTION->getText(), MCQ_TEXT);
-}
