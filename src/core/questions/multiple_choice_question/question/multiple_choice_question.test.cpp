7290d5d2d31ea4e03f09ba1f579fe94611a93cb0
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-10 13:58:20 +0200

    Modify MultipleChoiceQuestion unit tests to debug the CI

--- src/core/questions/multiple_choice_question/question/multiple_choice_question.test.cpp
+++ src/core/questions/multiple_choice_question/question/multiple_choice_question.test.cpp
@@ -1,19 +1,26 @@
 #include "builder/multiple_choice_question.builder.h"
 
-// Important Note: use clone() to get a new instance of the QUESTION to prevent modifying the
-// original one
+auto getCorrectAnswerInstance() {
+    return MultipleChoiceAnswerBuilder::create()
+        .setText(MCQ_CORRECT_ANSWER_TEXT)
+        .setDegree(MCQ_CORRECT_ANSWER_DEGREE)
+        .build();
+}
 
-// Static const variables
-static const auto CORRECT_ANSWER = MultipleChoiceAnswerBuilder::create()
-                                       .setText(MCQ_CORRECT_ANSWER_TEXT)
-                                       .setDegree(MCQ_CORRECT_ANSWER_DEGREE)
-                                       .build();
-static const auto STUDENT_ANSWER = MultipleChoiceAnswerBuilder::create().build();
-static const uint8_t TEXT_NEW = 1;
-static const double DEGREE_NEW = 5.0;
-static const auto QUESTION = MultipleChoiceQuestionBuilder::create()
-                                 .setText(MCQ_TEXT)
-                                 .setCorrectAnswer(std::move(CORRECT_ANSWER->clone()))
-                                 .setStudentAnswer(std::move(STUDENT_ANSWER->clone()))
-                                 .setChoices(CHOICES)
-                                 .build();
+auto getStudentAnswerInstance() {
+    return MultipleChoiceAnswerBuilder::create().build();
+}
+
+auto getQestionInstance() {
+    return MultipleChoiceQuestionBuilder::create()
+        .setText(MCQ_TEXT)
+        .setCorrectAnswer(std::move(getCorrectAnswerInstance()))
+        .setStudentAnswer(std::move(getStudentAnswerInstance()))
+        .setChoices(CHOICES)
+        .build();
+}
+
+TEST(GetTextTest, should_return_question_text_when_text_is_set) {
+    auto question = getQestionInstance();
+    EXPECT_EQ(question->getText(), MCQ_TEXT);
+}
