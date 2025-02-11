095d153501c5bb43305bec74c7c0a0a30674a085
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-02-11 10:22:43 +0200

    Fix warning: cast from type ‘const char*’ to type ‘char*’ casts away qualifiers [-Wcast-qual]

--- src/shared/utils/utils.test.cpp
+++ src/shared/utils/utils.test.cpp
@@ -9,21 +9,21 @@ TEST(GetAppRunModeTest, given_no_argv_then_returns_normal) {
 
 TEST(GetAppRunModeTest, given_argv_with_test_then_returns_test) {
     int argc = 2;
-    char* argv[2] = {(char*) "", (char*) "test"};
+    char* argv[2] = {const_cast<char*>(""), const_cast<char*>("test")};
 
     EXPECT_EQ(RunMode::TEST, Utils::getAppRunMode(argc, argv));
 }
 
 TEST(GetAppRunModeTest, given_argv_with_generate_questions_then_returns_generate_questions) {
     int argc = 2;
-    char* argv[2] = {(char*) "", (char*) "generate_questions"};
+    char* argv[2] = {const_cast<char*>(""), const_cast<char*>("generate_questions")};
 
     EXPECT_EQ(RunMode::GENERATE_QUESTIONS, Utils::getAppRunMode(argc, argv));
 }
 
 TEST(GetAppRunModeTest, given_argv_with_invalid_mode_then_returns_normal) {
     int argc = 2;
-    char* argv[2] = {(char*) "", (char*) "any other mode"};
+    char* argv[2] = {const_cast<char*>(""), const_cast<char*>("any other mode")};
 
     EXPECT_EQ(RunMode::NORMAL, Utils::getAppRunMode(argc, argv));
 }
